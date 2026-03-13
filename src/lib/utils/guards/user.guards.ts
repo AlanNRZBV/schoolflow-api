608f0d69d8b4d56c2cef96b791b28b1f0b25b010
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2026-03-13 16:35:48 +0600

    remove unused guards and update .gitignore

--- src/lib/utils/guards/user.guards.ts
+++
@@ -1,8 +0,0 @@
-import { CreatePersonnelDto } from '@/lib/guards/personnel.guard';
-import { CreateTeacherDto } from '@/models/user/teacher/teacher.types';
-import { CreateManagerDto } from '@/models/user/manager/manager.types';
-
-export const isTeacher = (arg: CreatePersonnelDto): arg is CreateTeacherDto =>
-	arg.role === 'teacher';
-export const isManager = (arg: CreatePersonnelDto): arg is CreateManagerDto =>
-	arg.role === 'manager';
